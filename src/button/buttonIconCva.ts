f61b340e171e02a773cbe27c724f778050c948fd
David Siewert <david1gruppenplan@gmail.com>
2025-09-20 09:41:22 +0600

    button / styling: * remove default * add/rename filled variants instead of warning/error/destructive

--- src/button/buttonIconCva.ts
+++ src/button/buttonIconCva.ts
@@ -1,6 +1,6 @@
 import { classArr } from "~/utils/ui/classArr"
 import { classMerge } from "~/utils/ui/classMerge"
-import { type ButtonVariant, buttonVariant } from "./buttonCva"
+import { type ButtonVariant } from "./buttonCva"
 
 const classesTextFillBlack = "text-black fill-black"
 const classesTextFillWhite = "text-white fill-white"
@@ -29,25 +29,24 @@ const buttonIconClasses = {
   // filled grayscale
   filled: classesBlackWhite,
   subtle: classesBlackWhite,
-  default: classArr(classesTextFillWhite, "dark:text-black dark:fill-black"),
+  contrast: classArr(classesTextFillWhite, "dark:text-black dark:fill-black"),
   // filled colors
-  primary: classArr(classesTextFillWhite, "dark:text-indigo-100 dark:fill-indigo-100"),
-  destructive: classesWhiteWhite,
-  warning: classesWhiteWhite,
-  success: classesWhiteWhite,
-  info: classesWhiteWhite,
+  filledYellow: classArr(classesTextFillWhite, "dark:text-yellow-100 dark:fill-yellow-100"),
+  filledOrange: classArr(classesTextFillWhite, "dark:text-orange-100 dark:fill-orange-100"),
+  filledAmber: classArr(classesTextFillWhite, "dark:text-amber-100 dark:fill-amber-100"),
+  filledRed: classArr(classesTextFillWhite, "dark:text-red-100 dark:fill-red-100"),
+
+  filledGreen: classesWhiteWhite,
+  filledIndigo: classArr(classesTextFillWhite, "dark:text-indigo-100 dark:fill-indigo-100"),
+  // outlineRed: classesWhiteWhite,
+  filledSky: classesWhiteWhite,
   // outlined colors
-  error: classArr(
-    "text-destructive dark:text-red-700", // text
-    "fill-destructive dark:fill-red-700", // fill
-  ),
+  outlineRed: classArr("text-red-500 fill-red-500 dark:text-red-700 dark:fill-red-700"),
 } as const satisfies Record<ButtonVariant, string>
 
-const defaultVariant = buttonVariant.default
-
 export function buttonIconCva(
-  variant: ButtonVariant | null = defaultVariant,
+  variant: ButtonVariant,
   ...customClasses: (string | boolean | undefined | null | 0 | 0n)[]
 ) {
-  return classMerge(baseClasses, buttonIconClasses[variant ?? defaultVariant], customClasses)
+  return classMerge(baseClasses, buttonIconClasses[variant], customClasses)
 }
