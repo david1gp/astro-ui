7f852c24470cece5b888eddf6a3c042a8df847e2
David Siewert <david1gruppenplan@gmail.com>
2025-09-20 09:42:04 +0600

    add select,  add select demo, add bg classes

--- src/demos/demoList.ts
+++ src/demos/demoList.ts
@@ -1,5 +1,5 @@
 import type { DemoListType } from "~/generate_demo_list/DemoListType"
 
 export const demoList = {
-  ui: ["demo_buttons", "demo_dialogs", "demo_popover"],
+  ui: ["demo_buttons", "demo_dialogs", "demo_popover", "demo_select"],
 } satisfies DemoListType
