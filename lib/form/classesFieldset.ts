f06c468bb6f5a17055348be8d57dd2720728b647
David Siewert <david1gruppenplan@gmail.com>
2025-09-25 14:53:23 +0600

    style(fieldset): increase brightness of border in dark mode

--- lib/form/classesFieldset.ts
+++ lib/form/classesFieldset.ts
@@ -1,1 +1,1 @@
-export const classesFieldset = "rounded-lg border border-solid border-gray-300 p-3"
+export const classesFieldset = "rounded-lg border border-solid border-gray-300 dark:border-gray-700 p-3"
