ce6afa61cb676ec666a46472f45c006086beb0bc
David Siewert <david1gruppenplan@gmail.com>
2025-10-03 19:45:06 +0600

    feat: add demos for all missing components

--- src/demos/generateDemoList.cli.ts
+++ src/demos/generateDemoList.cli.ts
@@ -1,7 +1,7 @@
 import { join } from "node:path"
 import { generateDemoList } from "~/generate_demo_list/generateDemoList"
 
-const searchPath = join(process.cwd(), "src/pages/demos")
-const outputPath = join(process.cwd(), "src/pages/demos/demoList.ts")
+const searchPath = join(process.cwd(), "src/pages")
+const outputPath = join(process.cwd(), "src/demos/demoList.ts")
 
 generateDemoList(searchPath, outputPath)
