0af75d330967994da73d4c7f28fc8023efb50d11
David Siewert <david1gruppenplan@gmail.com>
2025-10-03 19:45:06 +0600

    feat: add demos for all missing components

--- src/demos/demoList.ts
+++ src/demos/demoList.ts
@@ -1,5 +1,18 @@
 import type { DemoListType } from "~/generate_demo_list/DemoListType"
 
 export const demoList = {
-  ui: ["demo_buttons", "demo_dialogs", "demo_popover", "demo_select"],
+  select: ["demo_select"],
+  form: ["demo_fieldset"],
+  table: ["demo_table"],
+  img: ["demo_img", "demo_typed_img"],
+  layouts: ["demo_theme_toggle", "demo_markdown_wrapper"],
+  popover: ["demo_popover"],
+  modal: ["demo_modal", "demo_dialogs", "demo_modal_button"],
+  page: ["demo_page_centered", "demo_page_centered_card"],
+  card: ["demo_card"],
+  details: ["demo_details"],
+  list: ["demo_ps", "demo_check_point", "demo_check_points", "demo_numbered_list", "demo_black_bullet_points"],
+  link: ["demo_link_text", "demo_link_button"],
+  button: ["demo_buttons"],
+  icon: ["demo_icon"],
 } satisfies DemoListType
