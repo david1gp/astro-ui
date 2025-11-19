088939a61d002b4f1fc08a82b25fe50022f89dd0
David Siewert <david1gruppenplan@gmail.com>
2025-11-19 10:19:14 +0600

    refactor(tailwind): move global.css into src folder

--- lib/card/classesCardWrapper.ts
+++ lib/card/classesCardWrapper.ts
@@ -1,13 +1,13 @@
 import { classArr } from "~ui/utils/ui/classArr"
 
 export const classesRoundedShadowLg = "rounded-lg shadow-lg"
-export const classesCardWrapperBoderDark = "dark:border dark:border-gray-500"
+export const classesCardWrapperBorderDark = "dark:border dark:border-gray-500"
 export const classesCardWrapperBg = "bg-white dark:bg-zinc-800"
 
 export const classesCardWrapper = classArr(
   classesRoundedShadowLg, // card shadows/padding
   classesCardWrapperBg, // bg
-  classesCardWrapperBoderDark, // border
+  classesCardWrapperBorderDark, // border
 )
 
 export const classesCardWrapperP4 = classArr(classesCardWrapper, "p-4 lg:p-8")
@@ -20,5 +20,5 @@ export const classesCardWrapperPage = classArr(
   "p-4 sm:p-8 md:p-12", // padding
   // "sm:mx-auto", // center
   classesCardWrapperBg, // bg
-  classesCardWrapperBoderDark, // border
+  classesCardWrapperBorderDark, // border
 )
