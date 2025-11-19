87cda347b095b582d49bafdb7916aa84f17a0518
David Siewert <david1gruppenplan@gmail.com>
2025-11-19 09:47:30 +0600

    refactor(card): separate class parts

--- lib/card/classesCardWrapper.ts
+++ lib/card/classesCardWrapper.ts
@@ -1,10 +1,11 @@
 import { classArr } from "~ui/utils/ui/classArr"
 
+export const classesRoundedShadowLg = "rounded-lg shadow-lg"
 export const classesCardWrapperBoderDark = "dark:border dark:border-gray-500"
 export const classesCardWrapperBg = "bg-white dark:bg-zinc-800"
 
 export const classesCardWrapper = classArr(
-  "rounded-lg shadow-lg", // card shadows/padding
+  classesRoundedShadowLg, // card shadows/padding
   classesCardWrapperBg, // bg
   classesCardWrapperBoderDark, // border
 )
@@ -12,9 +13,12 @@ export const classesCardWrapper = classArr(
 export const classesCardWrapperP4 = classArr(classesCardWrapper, "p-4 lg:p-8")
 export const classesCardWrapperP8 = classArr(classesCardWrapper, "p-4 sm:p-8")
 
+export const classesRoundedShadowXl = "rounded-xl shadow-xl"
+
 export const classesCardWrapperPage = classArr(
-  "rounded-xl shadow-xl", // rounded border + shadow
+  classesRoundedShadowXl, // rounded border + shadow
   "p-4 sm:p-8 md:p-12", // padding
   // "sm:mx-auto", // center
-  "bg-white dark:bg-gray-900", // bg
+  classesCardWrapperBg, // bg
+  classesCardWrapperBoderDark, // border
 )
