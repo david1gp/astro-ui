1874b40e83de20614f30cf64d1bb1d1eb32be6c4
David Siewert <david1gruppenplan@gmail.com>
2026-01-07 13:54:31 +0600

    refactor(app): move layout, demos, layout_parts into app subfolder

--- src/app/demos/generateDemoList.cli.ts
+++ src/app/demos/generateDemoList.cli.ts
@@ -2,6 +2,6 @@ import { join } from "node:path"
 import { generateDemoList } from "~ui/generate_demo_list/generateDemoList"
 
 const searchPath = join(process.cwd(), "src/pages")
-const outputPath = join(process.cwd(), "src/demos/demoList.ts")
+const outputPath = join(process.cwd(), "src/app/demos/demoList.ts")
 
 generateDemoList(searchPath, outputPath)
